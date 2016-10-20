382a1d67fc77486608813c8b094bddd48d0b4f98
shopov91 <shopov91@gmail.com>
2016-10-20 15:29:50 +0300

    Find author

--- 02. Processing-XML-in-.NET/CatalogueManipulator/CatalogueManipulator/ArtistExtractor.cs
+++ 02. Processing-XML-in-.NET/CatalogueManipulator/CatalogueManipulator/ArtistExtractor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Xml;
 
 namespace CatalogueManipulator
@@ -10,20 +11,22 @@ namespace CatalogueManipulator
             XmlDocument doc = new XmlDocument();
             doc.Load("../../catalog.xml");
             XmlNode rootNode = doc.DocumentElement;
+            Hashtable artists = new Hashtable();
+            var i = 0;
 
             foreach (XmlNode node in rootNode.ChildNodes)
             {
                 foreach (XmlNode item in node)
                 {
-                    Console.WriteLine("Album name = {0}",
-                        item["name"].InnerText);
-                    Console.WriteLine("Artist name = {0}",
-                        item["artist"].InnerText);
-                    Console.WriteLine("Album price = {0}",
-                        item["price"].InnerText);
-                    Console.WriteLine();
+                    if (!artists.ContainsValue(item["artist"].InnerText))
+                    {
+                        artists.Add(i++, item["artist"].InnerText);
+                    }
                 }
-
+                foreach (var item in artists.Values)
+                {
+                    Console.WriteLine(item);
+                }
             }
         }
     }
