f70a9f37fb6f628883d908e06930b2e8d1600e3f
shopov91 <shopov91@gmail.com>
2016-10-20 14:53:25 +0300

    Added Task 2

---
+++ 02. Processing-XML-in-.NET/CatalogueManipulator/CatalogueManipulator/ArtistExtractor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Xml;
+
+namespace CatalogueManipulator
+{
+    public class ArtistExtractor
+    {
+        public void ExtractorMethod()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("../../catalog.xml");
+            XmlNode rootNode = doc.DocumentElement;
+
+            foreach (XmlNode node in rootNode.ChildNodes)
+            {
+                foreach (XmlNode item in node)
+                {
+                    Console.WriteLine("Album name = {0}",
+                        item["name"].InnerText);
+                    Console.WriteLine("Artist name = {0}",
+                        item["artist"].InnerText);
+                    Console.WriteLine("Album price = {0}",
+                        item["price"].InnerText);
+                    Console.WriteLine();
+                }
+
+            }
+        }
+    }
+}
---
+++ 02. Processing-XML-in-.NET/CatalogueManipulator/CatalogueManipulator/StartProgram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalogueManipulator
+{
+    public class StartProgram
+    {
+        public static void Main()
+        {
+            var extract = new ArtistExtractor();
+            extract.ExtractorMethod();
+        }
+    }
+}
