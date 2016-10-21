d891ff019a42ba03a9edd52edfa3e7bf5bc925a4
shopov91 <shopov91@gmail.com>
2016-10-21 10:56:34 +0300

    Update on task summery

--- 02. Processing-XML-in-.NET/CatalogueManipulator/CatalogueManipulator/StartProgram.cs
+++ 02. Processing-XML-in-.NET/CatalogueManipulator/CatalogueManipulator/StartProgram.cs
@@ -4,20 +4,29 @@
     {
         public static void Main()
         {
+            // task 1 and 2
             var domParsExtract = new DomParserExtractor();
             domParsExtract.DomParserExtractorMethod();
 
+            // task 3
             var xPathExtract = new XPathExtractor();
             xPathExtract.XPathExtractorMethod();
 
+            // task 4
             var albumDelete = new AlbumDeleter();
             albumDelete.AlbumDeleterMethod();
 
+            // task 5
             var xmlReaderTitleExtract = new XMLReaderSongTitleExtractor();
             xmlReaderTitleExtract.XMLReaderTitleExtractor();
 
+            // task 6
             var xDocLinqTitleExtract = new XDocumentAndLINQQueryTitleExtractor();
             xDocLinqTitleExtract.XDocumentAndLINQQuerySongTitleExtractor();
+
+            // task 8
+            var albumsCreator = new XmlReaderAndXmlWriter();
+            albumsCreator.CreateAlbumsXml();  
         }
     }
 }
--- 02. Processing-XML-in-.NET/CatalogueManipulator/Phonebook/Start.cs
+++ 02. Processing-XML-in-.NET/CatalogueManipulator/Phonebook/Start.cs
@@ -4,6 +4,7 @@
     {
        public static void Main()
         {
+            // task 7
             var phonebook = new Phonebook();
             phonebook.CreatePhonebook();
         }
