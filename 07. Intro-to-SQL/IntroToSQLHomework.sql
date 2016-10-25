c8e88891465c994dafa723e3d210713a7d0f6ab0
shopov91 <shopov91@gmail.com>
2016-10-25 19:46:42 +0300

    Adding tasks 1 to 3

---
+++ 07. Intro-to-SQL/IntroToSQLHomework.sql
@@ -0,0 +1,15 @@
+-- 1. Structured Query Language) is a special-purpose programming language designed for managing data
+-- held in a relational database management system (RDBMS)
+-- DML is abbreviation of Data Manipulation Language. It is used to retrieve, store, 
+-- modify, delete, insert and update data in database.
+-- Examples: SELECT, UPDATE, INSERT statements
+-- DDL is abbreviation of Data Definition Language. It is used to create and modify the structure 
+-- of database objects in database.
+-- Examples: CREATE, ALTER, DROP statements
+
+-- 2. Transact-SQL (T-SQL) is Microsoft's and Sybase's proprietary extension to SQL. SQL, 
+-- the acronym for Structured Query Language, is a standardized computer language that was 
+-- originally developed by IBM for querying, altering and defining relational databases, 
+-- using declarative statements.
+
+-- Queries can be executed one at the time by selecting the relevant text and pressing F5
