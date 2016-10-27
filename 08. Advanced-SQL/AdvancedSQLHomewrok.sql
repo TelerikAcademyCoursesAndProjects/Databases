ee2148ad31fc58be99d9ae73fb2b0970ffdb6760
shopov91 <shopov91@gmail.com>
2016-10-27 16:22:45 +0300

    Added task 15 and 16

--- 08. Advanced-SQL/AdvancedSQLHomewrok.sql
+++ 08. Advanced-SQL/AdvancedSQLHomewrok.sql
@@ -109,3 +109,29 @@ WHERE LEN(e.LastName) = 5
 -- 14. Write a SQL query to display the current date and time in the following format "day.month.year hour:minutes:seconds:milliseconds".
 
 SELECT CONVERT(VARCHAR, GETDATE(), 113) AS 'Current Data And Time'
+
+-- 15. Write a SQL statement to create a table Users. Users should have username, password, full name and last login time.
+-- Choose appropriate data types for the table fields. Define a primary key column with a primary key constraint.
+-- Define the primary key column as identity to facilitate inserting records.
+-- Define unique constraint to avoid repeating usernames.
+-- Define a check constraint to ensure the password is at least 5 characters long.
+
+CREATE TABLE Users (
+	UserId INT IDENTITY,
+	Username NVARCHAR(30) UNIQUE NOT NULL,
+	[Password] NVARCHAR(30) CHECK (DATALENGTH([Password]) >= 5) NOT NULL,
+	FullName NVARCHAR(100),
+	LoginTime DATETIME,
+	CONSTRAINT PK_Users PRIMARY KEY(UserId)
+)
+GO
+
+-- 16. Write a SQL statement to create a view that displays the users from the Users table that have been in the system today.
+-- Test if the view works correctly.
+
+CREATE VIEW [Recently Logged] AS
+SELECT Username, LoginTime
+FROM Users
+WHERE
+CONVERT(VARCHAR(10), LoginTime, 102) <= CONVERT(VARCHAR(10) ,GETDATE(), 102)
+GO
