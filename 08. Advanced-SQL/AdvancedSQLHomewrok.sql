6e8eef20f4348f27e9d21d6fd2577ded748e3197
shopov91 <shopov91@gmail.com>
2016-10-27 13:57:42 +0300

    Added Advanced-SQL README.md

---
+++ 08. Advanced-SQL/AdvancedSQLHomewrok.sql
@@ -0,0 +1,64 @@
+USE TelerikAcademy
+-- 1. Write a SQL query to find the names and salaries of the employees that take the minimal salary in the company.
+-- Use a nested SELECT statement.
+
+SELECT FirstName,LastName,Salary
+FROM Employees
+WHERE Salary=
+	(SELECT MIN(Salary) FROM	Employees)
+
+-- 2. Write a SQL query to find the names and salaries of the employees that have a salary that is up to 10% higher than the minimal salary for the company.
+
+SELECT FirstName, LastName, Salary
+FROM Employees
+WHERE Salary <
+	(SELECT MIN(Salary) FROM Employees) * 1.1
+ORDER BY Salary 
+
+-- 3. Write a SQL query to find the full name, salary and department of the employees that take the minimal salary in their department.
+-- Use a nested SELECT statement.
+
+SELECT e.FirstName + ' ' + e.LastName AS 'Full Name', e.Salary, d.Name AS 'Department'
+FROM Employees e
+	JOIN Departments d
+		ON e.DepartmentID = d.DepartmentID
+WHERE Salary =
+	(SELECT MIN(Salary) 
+	 FROM Employees
+	 WHERE DepartmentID = e.DepartmentID)
+ORDER BY Salary
+
+-- 4. Write a SQL query to find the average salary in the department #1.
+
+SELECT AVG(e.Salary) AS 'Department 1 Average Salary'
+FROM Employees e
+WHERE e.DepartmentID = 1
+
+-- 5. Write a SQL query to find the average salary in the "Sales" department.
+
+SELECT AVG(e.Salary) AS 'Average Salary In "Sales" Department'
+FROM Employees e
+	JOIN Departments d
+		ON e.DepartmentID = d.DepartmentID
+WHERE d.Name= 'Sales' 
+
+-- 6. Write a SQL query to find the number of employees in the "Sales" department.
+
+SELECT COUNT(*) AS 'Number Of Employees In "Sales" Department'
+FROM Employees e
+	JOIN Departments d
+		ON e.DepartmentID = d.DepartmentID
+WHERE d.Name= 'Sales' 
+
+-- 7. Write a SQL query to find the number of all employees that have manager.
+
+SELECT COUNT(e.ManagerID) AS 'Number Of Employees That Have Manager'
+FROM Employees e
+WHERE e.ManagerID IS NOT NULL -- moje i bez tozi posledniqt red poneje po count-a ni vra6ta vsi4ki stoinosti bez NULL!!!
+
+-- 7. Write a SQL query to find the number of all employees that have no manager.
+
+SELECT COUNT(*) AS 'Number Of Employees That Don''t Have Manager'
+FROM Employees e
+WHERE e.ManagerID IS NULL
+
