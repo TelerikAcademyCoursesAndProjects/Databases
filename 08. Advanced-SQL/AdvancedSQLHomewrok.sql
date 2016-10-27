a95f6f29b407058c4d71a0b5fceef96bd2666011
shopov91 <shopov91@gmail.com>
2016-10-27 15:29:20 +0300

    Added task 11

--- 08. Advanced-SQL/AdvancedSQLHomewrok.sql
+++ 08. Advanced-SQL/AdvancedSQLHomewrok.sql
@@ -82,3 +82,12 @@ FROM Employees e
 		ON e.DepartmentID = d.DepartmentID
 GROUP BY d.Name, t.Name
 ORDER BY d.Name
+
+-- 11. Write a SQL query to find all managers that have exactly 5 employees. Display their first name and last name.
+
+SELECT m.FirstName + ' ' + m.LastName AS 'Manager Full Name'
+FROM Employees e
+	JOIN Employees m
+		ON e.ManagerID = m.EmployeeID
+GROUP BY m.FirstName, m.LastName
+HAVING COUNT(m.EmployeeID) = 5
