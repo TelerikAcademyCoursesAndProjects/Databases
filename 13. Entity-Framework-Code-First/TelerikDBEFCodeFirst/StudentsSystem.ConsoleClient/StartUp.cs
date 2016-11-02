2905d48b2a51ca1d423967fc33db8dba122b612e
shopov91 <shopov91@gmail.com>
2016-11-02 15:37:05 +0200

    Added new console clien project

---
+++ 13. Entity-Framework-Code-First/TelerikDBEFCodeFirst/StudentsSystem.ConsoleClient/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsSystem.ConsoleClient
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+        }
+    }
+}
