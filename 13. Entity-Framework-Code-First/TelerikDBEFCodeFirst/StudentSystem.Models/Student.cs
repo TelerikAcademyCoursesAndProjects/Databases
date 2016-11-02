52e97c49ed6760df34e3af034832c2c681d2971e
shopov91 <shopov91@gmail.com>
2016-11-02 11:40:14 +0200

    Class Student added

--- 13. Entity-Framework-Code-First/TelerikDBEFCodeFirst/StudentSystem.Models/Student.cs
+++ 13. Entity-Framework-Code-First/TelerikDBEFCodeFirst/StudentSystem.Models/Student.cs
@@ -8,5 +8,10 @@ namespace StudentSystem.Models
 {
     public class Student
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Number { get; set; }
     }
 }
