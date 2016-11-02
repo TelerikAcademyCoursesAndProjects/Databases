1e2c6cc3285a5300f0a98f851867097e810b3139
shopov91 <shopov91@gmail.com>
2016-11-02 15:20:05 +0200

    Added EF to the project

--- 13. Entity-Framework-Code-First/TelerikDBEFCodeFirst/StudentSystem.Data/StudentDbContext.cs
+++ 13. Entity-Framework-Code-First/TelerikDBEFCodeFirst/StudentSystem.Data/StudentDbContext.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StudentSystem.Data
+﻿namespace StudentSystem.Data
 {
-    public class Class1
+    public class StudentDbContext
     {
+
     }
 }
