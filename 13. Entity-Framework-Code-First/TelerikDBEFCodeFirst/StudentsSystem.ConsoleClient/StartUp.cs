5ae419616edae3c82ee63a1e2690ee3317c5a880
shopov91 <shopov91@gmail.com>
2016-11-02 15:43:49 +0200

    Added EF for all projects in the solution

--- 13. Entity-Framework-Code-First/TelerikDBEFCodeFirst/StudentsSystem.ConsoleClient/StartUp.cs
+++ 13. Entity-Framework-Code-First/TelerikDBEFCodeFirst/StudentsSystem.ConsoleClient/StartUp.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StudentsSystem.ConsoleClient
+﻿namespace StudentsSystem.ConsoleClient
 {
     public class StartUp
     {
