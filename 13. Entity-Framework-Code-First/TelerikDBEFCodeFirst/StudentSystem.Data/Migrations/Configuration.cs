95b0e819603e714ec51ebab4dd2767da9f78d335
shopov91 <shopov91@gmail.com>
2016-11-05 18:26:32 +0200

    Update on configuration.cs the seed method

--- 13. Entity-Framework-Code-First/TelerikDBEFCodeFirst/StudentSystem.Data/Migrations/Configuration.cs
+++ 13. Entity-Framework-Code-First/TelerikDBEFCodeFirst/StudentSystem.Data/Migrations/Configuration.cs
@@ -1,4 +1,4 @@
-namespace StudentSystem.Data.Migrations
+﻿namespace StudentSystem.Data.Migrations
 {
     using System;
     using Models;
@@ -14,19 +14,21 @@ namespace StudentSystem.Data.Migrations
 
         protected override void Seed(StudentDbContext context)
         {
-            context.Students
-                 .AddOrUpdate(
-                s => s.Name,
-                new Student
-                {
-                    Name = "First seeded student",
-                    Number = 13131,
-                    Information = new StudentAdditionalInfo
-                    {
-                        Age = new Random().Next(18, 120),
-                        Country = "Bulgaria"
-                    }
-                });
+            // ТОВА СЕ РАЗКОМЕНТИРА И ИЗПОЛЗВА ЗА ТЕСТОВИ ЦЕЛИ,ТО НИ ВЪВЕЖДА ДЕФОУТНА СТОЙНОСТ ПРИ СЪЗДАВАНЕ НА НОВА БАЗА(КАТО ЦЯЛО ТОЗИ SEED МЕТОД)
+            // СЛЕД КАТО СМЕ СИГУРНИ, ЧЕ СИ БАЧКА НЕ НИ ТРЯБВА ДА НИ ПЪЛНИ НИЩО ТОЙ И ЗАТОВА СИ ЗАКОМЕНТИРАМЕ!!!!!)
+            //context.Students
+            //     .AddOrUpdate(
+            //    s => s.Name,
+            //    new Student
+            //    {
+            //        Name = "First seeded student",
+            //        Number = 13131,
+            //        Information = new StudentAdditionalInfo
+            //        {
+            //            Age = new Random().Next(18, 120),
+            //            Country = "Bulgaria"
+            //        }
+            //    });
         }
     }
 }
